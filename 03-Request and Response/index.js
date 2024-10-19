3457d29bcd75771852a3e3165fc06710a25280fb
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-20 00:58:04 +0600

    Request and Response

--- 03-Request and Response/index.js
+++ 03-Request and Response/index.js
@@ -1,22 +1,26 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
+
     if(req.url=="/"){
         res.writeHead(200,{'Conent-Type':'text/html'})
         res.write('<h1>This is Home Page</h1>')
         res.end();
     }
+
     else if(req.url=="/about"){
         res.writeHead(200,{'Conent-Type':'text/html'})
         res.write('<h1>This is About Page</h1>')
         res.end();
     }
+
     else if(req.url=="/contact"){
         res.writeHead(200,{'Conent-Type':'text/html'})
         res.write('<h1>This is Contact Page</h1>')
         res.end();
     }
 });
+
 
 server.listen(4000);
 console.log("server Run success");
